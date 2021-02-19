3c8bc26c9131bae7aa9de67d8c8b6c5d5fc47b4b
KevinTraan <71052182+KevinTraan@users.noreply.github.com>
2021-02-19 15:19:11 -0500

    Update Mux32to1.v

--- phase1-2020/Mux32to1.v
+++ phase1-2020/Mux32to1.v
@@ -4,13 +4,11 @@ module Mux32to1 (
 
   input [31:0] R0MuxIn, R1MuxIn, R2MuxIn, R3MuxIn, R4MuxIn, R5MuxIn, R6MuxIn, R7MuxIn, R8MuxIn, R9MuxIn, R10MuxIn, R11MuxIn, R12MuxIn, R13MuxIn, R14MuxIn, R15MuxIn,
 
-  input [31:0] HIMuxIn, LOMuxIn, zhighMuxIn, zlowMuxIn, PCMuxIn, MDRMuxIn, InPortMuxIn, CMuxin,
+  input [31:0] HIMuxIn, LOMuxIn, zhighMuxIn, zlowMuxIn, PCMuxIn, MDRMuxIn, InPortMuxIn, CMuxIn,
 
   input reg [4:0] Scode,
 
-  output [31:0] BusMuxOut
-
-);
+  output reg [31:0] BusMuxOut);
 
 /*
   Always block runs whenever any variable changes.
@@ -20,54 +18,54 @@ module Mux32to1 (
 
 always @ ( * ) begin
 
-if (Scode == 5'b00000)
+  if (Scode == 5'b0)
   BusMuxOut <= R0MuxIn;
-else if (Scode == 5'b00001)
+  else if (Scode == 5'b0_0001)
   BusMuxOut <= R1MuxIn;
-else if (Scode == 5'b00010)
+  else if (Scode == 5'b0_0010)
   BusMuxOut <= R2MuxIn;
-else if (Scode == 5'b00011)
+  else if (Scode == 5'b0_0011)
   BusMuxOut <= R3MuxIn;
-else if (Scode == 5'b00100)
+  else if (Scode == 5'b0_0100)
   BusMuxOut <= R4MuxIn;
-else if (Scode == 5'b00101)
+  else if (Scode == 5'b0_0101)
   BusMuxOut <= R5MuxIn;
-else if (Scode == 5'b00110)
+  else if (Scode == 5'b0_0110)
   BusMuxOut <= R6MuxIn;
-else if (Scode == 5'b00111)
+  else if (Scode == 5'b0_0111)
   BusMuxOut <= R7MuxIn;
-else if (Scode == 5'b01000)
+  else if (Scode == 5'b0_1000)
   BusMuxOut <= R8MuxIn;
-else if (Scode == 5'b01001)
+  else if (Scode == 5'b0_1001)
   BusMuxOut <= R9MuxIn;
-else if (Scode == 5'b01010)
+  else if (Scode == 5'b0_1010)
   BusMuxOut <= R10MuxIn;
-else if (Scode == 5'b01011)
+  else if (Scode == 5'b0_1011)
   BusMuxOut <= R11MuxIn;
-else if (Scode == 5'b01100)
+  else if (Scode == 5'b0_1100)
   BusMuxOut <= R12MuxIn;
-else if (Scode == 5'b01101)
+  else if (Scode == 5'b0_1101)
   BusMuxOut <= R13MuxIn;
-else if (Scode == 5'b01110)
+  else if (Scode == 5'b0_1110)
   BusMuxOut <= R14MuxIn;
-else if (Scode == 5'b01111)
+  else if (Scode == 5'b0_1111)
   BusMuxOut <= R15MuxIn;
 
-else if (Scode == 5'b10000)
+  else if (Scode == 5'b1_0001)
   BusMuxOut <= HIMuxIn;
-else if (Scode == 5'b10001)
+  else if (Scode == 5'b1_0010)
   BusMuxOut <= LOMuxIn;
-else if (Scode == 5'b10010)
+  else if (Scode == 5'b1_0011)
   BusMuxOut <= zhighMuxIn;
-else if (Scode == 5'b10011)
+  else if (Scode == 5'b1_0100)
   BusMuxOut <= zlowMuxIn;
-else if (Scode == 5'b10100)
+  else if (Scode == 5'b1_0101)
   BusMuxOut <= PCMuxIn;
-else if (Scode == 5'b10101)
+  else if (Scode == 5'b1_0110)
   BusMuxOut <= MDRMuxIn;
-else if (Scode == 5'b10110)
+  else if (Scode == 5'b1_0111)
   BusMuxOut <= InPortMuxIn;
-else if (Scode == 5'b10111)
+  else if (Scode == 5'b1_1000)
   BusMuxOut <= CMuxIn;
 
 end
