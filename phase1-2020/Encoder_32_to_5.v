a5bb54ab3ea68c93a8a609e2923586d22cd8f319
Samir <17st54@queensu.ca>
2021-02-22 13:03:08 -0500

    Fix counting error in bus

--- phase1-2020/Encoder_32_to_5.v
+++ phase1-2020/Encoder_32_to_5.v
@@ -18,9 +18,9 @@ always @ ( * )
   begin
 
     if (R0out == 1'b1)
-      Sout <= 5'b0;
+      Sout <= 5'b0_0000;
     else if (R1out == 1'b1)
-      Sout <= 5'b1;
+      Sout <= 5'b0_0001;
     else if (R2out == 1'b1)
       Sout <= 5'b0_0010;
     else if (R3out == 1'b1)
@@ -28,44 +28,44 @@ always @ ( * )
     else if (R4out == 1'b1)
       Sout <= 5'b0_0100;
     else if (R5out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_0101;
     else if (R6out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_0110;
     else if (R7out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_0111;
     else if (R8out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1000;
     else if (R9out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1001;
     else if (R10out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1010;
     else if (R11out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1011;
     else if (R12out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1100;
     else if (R13out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1101;
     else if (R14out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1110;
     else if (R15out == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b0_1111;
 
     else if (HIout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0000;
     else if (LOout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0001;
     else if (zhighout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0010;
     else if (zlowout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0011;
     else if (PCout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0100;
     else if (MDRout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0101;
     else if (InPortout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0110;
     else if (Cout == 1'b1)
-      Sout <= 5'b0_0000;
+      Sout <= 5'b1_0111;
 
   end
 
--- phase1-2020/Mux32to1.v
+++ phase1-2020/Mux32to1.v
@@ -16,58 +16,57 @@ module Mux32to1 (
   output value of register that sCode designates.
 */
 
-always @ ( * ) begin
+  always @ ( * ) begin
 
-  if (Scode == 5'b0)
-  BusMuxOut <= R0MuxIn;
-  else if (Scode == 5'b0_0001)
-  BusMuxOut <= R1MuxIn;
-  else if (Scode == 5'b0_0010)
-  BusMuxOut <= R2MuxIn;
-  else if (Scode == 5'b0_0011)
-  BusMuxOut <= R3MuxIn;
-  else if (Scode == 5'b0_0100)
-  BusMuxOut <= R4MuxIn;
-  else if (Scode == 5'b0_0101)
-  BusMuxOut <= R5MuxIn;
-  else if (Scode == 5'b0_0110)
-  BusMuxOut <= R6MuxIn;
-  else if (Scode == 5'b0_0111)
-  BusMuxOut <= R7MuxIn;
-  else if (Scode == 5'b0_1000)
-  BusMuxOut <= R8MuxIn;
-  else if (Scode == 5'b0_1001)
-  BusMuxOut <= R9MuxIn;
-  else if (Scode == 5'b0_1010)
-  BusMuxOut <= R10MuxIn;
-  else if (Scode == 5'b0_1011)
-  BusMuxOut <= R11MuxIn;
-  else if (Scode == 5'b0_1100)
-  BusMuxOut <= R12MuxIn;
-  else if (Scode == 5'b0_1101)
-  BusMuxOut <= R13MuxIn;
-  else if (Scode == 5'b0_1110)
-  BusMuxOut <= R14MuxIn;
-  else if (Scode == 5'b0_1111)
-  BusMuxOut <= R15MuxIn;
+    if (Scode == 5'b0_0000)
+      BusMuxOut <= R0MuxIn;
+    else if (Scode == 5'b0_0001)
+      BusMuxOut <= R1MuxIn;
+    else if (Scode == 5'b0_0010)
+      BusMuxOut <= R2MuxIn;
+    else if (Scode == 5'b0_0011)
+      BusMuxOut <= R3MuxIn;
+    else if (Scode == 5'b0_0100)
+      BusMuxOut <= R4MuxIn;
+    else if (Scode == 5'b0_0101)
+      BusMuxOut <= R5MuxIn;
+    else if (Scode == 5'b0_0110)
+      BusMuxOut <= R6MuxIn;
+    else if (Scode == 5'b0_0111)
+      BusMuxOut <= R7MuxIn;
+    else if (Scode == 5'b0_1000)
+      BusMuxOut <= R8MuxIn;
+    else if (Scode == 5'b0_1001)
+      BusMuxOut <= R9MuxIn;
+    else if (Scode == 5'b0_1010)
+      BusMuxOut <= R10MuxIn;
+    else if (Scode == 5'b0_1011)
+      BusMuxOut <= R11MuxIn;
+    else if (Scode == 5'b0_1100)
+      BusMuxOut <= R12MuxIn;
+    else if (Scode == 5'b0_1101)
+      BusMuxOut <= R13MuxIn;
+    else if (Scode == 5'b0_1110)
+      BusMuxOut <= R14MuxIn;
+    else if (Scode == 5'b0_1111)
+      BusMuxOut <= R15MuxIn;
 
-  else if (Scode == 5'b1_0001)
-  BusMuxOut <= HIMuxIn;
-  else if (Scode == 5'b1_0010)
-  BusMuxOut <= LOMuxIn;
-  else if (Scode == 5'b1_0011)
-  BusMuxOut <= zhighMuxIn;
-  else if (Scode == 5'b1_0100)
-  BusMuxOut <= zlowMuxIn;
-  else if (Scode == 5'b1_0101)
-  BusMuxOut <= PCMuxIn;
-  else if (Scode == 5'b1_0110)
-  BusMuxOut <= MDRMuxIn;
-  else if (Scode == 5'b1_0111)
-  BusMuxOut <= InPortMuxIn;
-  else if (Scode == 5'b1_1000)
-  BusMuxOut <= CMuxIn;
-
-end
+    else if (Scode == 5'b1_0000)
+      BusMuxOut <= HIMuxIn;
+    else if (Scode == 5'b1_0001)
+      BusMuxOut <= LOMuxIn;
+    else if (Scode == 5'b1_0010)
+      BusMuxOut <= zhighMuxIn;
+    else if (Scode == 5'b1_0011)
+      BusMuxOut <= zlowMuxIn;
+    else if (Scode == 5'b1_0100)
+      BusMuxOut <= PCMuxIn;
+    else if (Scode == 5'b1_0101)
+      BusMuxOut <= MDRMuxIn;
+    else if (Scode == 5'b1_0110)
+      BusMuxOut <= InPortMuxIn;
+    else if (Scode == 5'b1_0111)
+      BusMuxOut <= CMuxIn;
+  end
 
 endmodule
