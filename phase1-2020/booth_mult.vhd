8a66ee2a8db4d1fe0217d7f91eb01a1124ee0239
Alex McKinnon <alex@mckinnon.io>
2021-02-22 18:41:43 -0500

    updated, closer but not working

--- phase1-2020/booth_mult.vhd
+++ phase1-2020/booth_mult.vhd
@@ -10,14 +10,13 @@ module booth_mult (
 
 integer i = 0;
 
-reg [31:0] mult_neg = -mult;
+reg [31:0] mult_neg;
 reg [63:0] P; // answer store temp
 reg [63:0] A;
 reg [63:0] S;
 reg [63:0] prod;
 
-reg leftMost;
-reg rightMost
+reg [1:0] aRight, sRight, pRight, prodRight;
 
 // A = mult_pos
 // S = mult_neg
@@ -28,15 +27,24 @@ always @(*) begin
 	// assign values
     
     mult_neg = -mult;
-    A = { mult[31], mult[31:0], 33'b0 }; // sign extend mult, add extra end bit
-    S = { mult_neg[31], mult_neg[31:0], 33'b0 }; // invert sign extended 64 bit
-    prod = { 33'b0, multr[31:0], 1'b0 }; //export answer
+
+	aRight = 2'b0;	// add extra end bit
+    A = {mult[31:0], 32'h0 }; // sign extend mult, 
+    
+    sRight = 2'b0;
+    S = { mult_neg[31:0], 32'b0 }; // invert sign extended 64 bit
+    
+    prodRight = {multr[0], 1'b0};
+    prod = { 33'b0, multr[31:1] }; //export answer
     
     
-    for (i = 0; i < 32; i = i + 1) begin
+    for (i = 0; i < 4; i = i + 1) begin
     	    P <= prod;
-        case ({P[1:0]})
+            pRight <= prodRight;
+            
+        case ({pRight[1:0]})
 			2'b00: begin 
+              prodRight = {P[0], pRight[1]};
               prod <= P >>> 1;
             end
             2'b01: begin
@@ -46,15 +54,19 @@ always @(*) begin
               prod <= (P + S) >>> 1;
             end
             2'b11: begin
+              prodRight = {P[0], pRight[1]};
               prod <= P >>> 1;
 			end
             
         endcase
     end
+    
+    P = {prod[30:0], prodRight[1]};
+    
 end
 
 
-assign ZhighOut = prod[31:0]; 
-assign ZlowOut = prod[63:32]; 
+assign ZhighOut = P[31:0]; 
+assign ZlowOut = P[63:32]; 
 
 endmodule
--- phase1-2020/booth_mult_TB.svh
+++ phase1-2020/booth_mult_TB.svh
@@ -7,25 +7,24 @@ module booth_mult_TB;
   wire [31:0] ZhighOut;
   wire [31:0] ZlowOut;
 
-  
-
-initial begin
-    $dumpfile("out.vcd");
-    $dumpvars(0, mult_test);
-
-    mult <= 31'h3;
-    multr <= 31'h8;
-    #40
-	$finish;
-end
-  
-    booth_mult mult_test(
+  booth_mult mult_test(
 	mult,
 	multr,
 	ZhighOut,
     ZlowOut
-    );
+  );
 
+
+
+initial begin
+    $dumpfile("out.vcd");
+    $dumpvars(0, mult_test);
+    mult <= 31'hFFFFFFFF;
+    multr <= 31'd4;
+    #40
+	$finish;
+end
+  
 
 
 endmodule
