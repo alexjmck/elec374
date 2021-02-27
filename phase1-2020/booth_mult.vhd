50ffddd9a5bb83eb52ed3a5d601dce1ef046d312
Alex McKinnon <alex@mckinnon.io>
2021-02-27 12:07:50 -0500

    Update booth_mult.vhd

--- phase1-2020/booth_mult.vhd
+++ phase1-2020/booth_mult.vhd
@@ -1,72 +1,70 @@
 `timescale 1ns / 1ps
 
 module booth_mult (
-  input [31:0] mult,
-  input [31:0] multr,
-  output reg [31:0] ZhighOut,
-  output reg [31:0] ZlowOut
+    input [31:0] mult,
+    input [31:0] multr,
+    output reg [31:0] ZhighOut,
+    output reg [31:0] ZlowOut
 
-);
+  );
 
-integer i = 0;
+  integer i;
+  reg cb; //carry bit
+  reg [1:0] mb;
 
-reg [31:0] mult_neg;
-reg [63:0] P; // answer store temp
-reg [63:0] A;
-reg [63:0] S;
-reg [63:0] prod;
-
-reg [1:0] aRight, sRight, pRight, prodRight;
-
-// A = mult_pos
-// S = mult_neg
-// P = multr
-
-always @(*) begin
-
-	// assign values
-    
-    mult_neg = -mult;
-
-	aRight = 2'b0;	// add extra end bit
-    A = {mult[31:0], 32'h0 }; // sign extend mult, 
-    
-    sRight = 2'b0;
-    S = { mult_neg[31:0], 32'b0 }; // invert sign extended 64 bit
-    
-    prodRight = {multr[0], 1'b0};
-    prod = { 33'b0, multr[31:1] }; //export answer
-    
-    
-    for (i = 0; i < 4; i = i + 1) begin
-    	    P <= prod;
-            pRight <= prodRight;
-            
-        case ({pRight[1:0]})
-			2'b00: begin 
-              prodRight = {P[0], pRight[1]};
-              prod <= P >>> 1;
-            end
-            2'b01: begin
-              prod <= (P + A) >>> 1;
-            end
-            2'b10: begin
-              prod <= (P + S) >>> 1;
-            end
-            2'b11: begin
-              prodRight = {P[0], pRight[1]};
-              prod <= P >>> 1;
-			end
-            
-        endcase
-    end
-    
-    P = {prod[30:0], prodRight[1]};
-    
-end
+  reg [33:0] P1; // answer store temp
+  reg [33:0] P2; 
+  reg [63:0] prod;
+  
+  initial begin
+  		cb = 0;
+        prod = 0;
+        ZhighOut = 0;
+        ZlowOut = 0;
+  end
+  
 
 
-assign ZhighOut = P[31:0]; 
-assign ZlowOut = P[63:32]; 
+  wire [33:0] mult_pos_1 = {mult[31], mult[31], mult};
+  wire [33:0] mult_neg_1 = -mult_pos_1;
+  wire [33:0] mult_pos_2 = { mult[31], mult, 1'b0 };
+  wire [33:0] mult_neg_2 = -mult_pos_2;
+
+  always @(*) begin
+
+
+      for (i = 0; i < 16; i = i + 1) begin
+          if (i==0)
+              prod = {32'd0, multr};
+
+
+          P1 = {prod[63], prod[63], prod[63:32]};
+
+          mb = prod[1:0];
+
+          case( {mb, cb} )
+              3'b000: P2 = P1;
+              3'b001: P2 = P1 + mult_pos_1;
+              3'b010: P2 = P1 + mult_pos_1;
+              3'b011: P2 = P1 + mult_pos_2;
+              3'b100: P2 = P1 + mult_neg_2;
+              3'b101: P2 = P1 + mult_neg_1;
+              3'b110: P2 = P1 + mult_neg_1;
+              3'b111: P2 = P1;
+          endcase
+
+
+          cb = prod[1];
+
+          prod = {P2, prod[31:2]};
+
+      end
+      ZhighOut = prod[31:0]; 
+      ZlowOut = prod[63:32]; 
+
+  end
+
+
+
 
 endmodule
